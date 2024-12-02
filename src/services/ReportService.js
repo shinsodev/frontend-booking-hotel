9a1d5336f58d32fe57d87e682411fe9950522e01
lucidjug <tuuju2@gmail.com>
2024-12-03 00:16:04 +0700

    update Report latest

---
+++ src/services/ReportService.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+export const api = axios.create({
+    baseURL: "http://localhost:8080",
+});
+export async function getReport(year) {
+    try {
+        const token = localStorage.getItem("token");
+        const result = await api.get("/reports", {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+            params: {
+                year
+            }
+        });
+        return result.data;
+    } catch (error) {
+        if (error.response && error.response.status === 403) {
+            console.error("Authorization error: Access denied (403)");
+        }
+        throw new Error("Error fetching reports ");
+    }
+}
